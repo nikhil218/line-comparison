f9efa4a33998ab4e8eb770842367dfcd63254180
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-30 23:52:15 +0530

    Added code to compare length of two lines

--- line.java
+++ line.java
@@ -1,52 +1,60 @@
 import java.util.Scanner;
 
 public class line{
-   public double length_of_line(){
+	public double length_of_line(){
 
-      Scanner obj = new Scanner(System.in);
-      double x1, x2, y1, y2;
-      double length_of_line = 0.0;
+		Scanner obj = new Scanner(System.in);
+		double x1, x2, y1, y2;
+		double length_of_line = 0.0;
 
-      System.out.println("Enter x1 value : ");
-      x1 = obj.nextDouble();
+		System.out.println("Enter x1 value : ");
+		x1 = obj.nextDouble();
 
-      System.out.println("Enter y1 value : ");
-      y1 = obj.nextDouble();
+		System.out.println("Enter y1 value : ");
+		y1 = obj.nextDouble();
 
-      System.out.println("Enter x2 value : ");
-      x2 = obj.nextDouble();
+		System.out.println("Enter x2 value : ");
+		x2 = obj.nextDouble();
 
-      System.out.println("Enter y2 value : ");
-      y2 = obj.nextDouble();
+		System.out.println("Enter y2 value : ");
+		y2 = obj.nextDouble();
 
-      length_of_line = Math.sqrt( (Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)) );
-      return length_of_line;
-   }
+		length_of_line = Math.sqrt( (Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)) );
+		return length_of_line;
+	}
 
-   public static void main(String[] args){
+	public static void main(String[] args){
 
-      System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Welcome to Line Comparison Computation Program");
 
-      line l = new line();
-      System.out.println("Enter First Line Co-ordinates : ");
-      double first_line = l.length_of_line();
-      System.out.println("Length of first line is : " + first_line);
+		line l = new line();
+		System.out.println("Enter First Line Co-ordinates : ");
+		double first_line = l.length_of_line();
+		System.out.println("Length of first line is : " + first_line);
 
-      System.out.println("Enter second Line Co-ordinates : ");
+		System.out.println("Enter second Line Co-ordinates : ");
       double second_line = l.length_of_line();
       System.out.println("Length of first line is : " + second_line);
 
-      String first=Double.toString(first_line);
-      String second=Double.toString(second_line);
+		String first=Double.toString(first_line);
+		String second=Double.toString(second_line);
 
-      if(first.equals(second))
+		double com=first.compareTo(second);
+
+		if(com>=1)
       {
-         System.out.println("First line length is equals  to Second line");
+         System.out.println("First line length is greater than Second line");
+      }
+      else if(com<0)
+      {
+          System.out.println("Second line length is greater than First line");
+
       }
       else
       {
-          System.out.println("Both lines are not of equal lengths");
+          System.out.println("Both lines are of equal lengths");
       }
 
-   }
+	}
 }
+
